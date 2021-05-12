e1b6ae5a605f575ee4459eb6010eba2dfbb86be3
Gonzalo Perez-Paina <gfperezpaina@gmail.com>
2021-05-12 10:14:40 -0300

    Agrega script y archivo de lanzamiento

---
+++ script/odom_scan_log.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python
+
+import os
+import rospy
+from nav_msgs.msg import Odometry
+from sensor_msgs.msg import LaserScan
+from tf.transformations import euler_from_quaternion
+from std_srvs.srv import SetBool
+
+class OdomScanLog():
+
+  def __init__(self):
+
+    self.srv_name = 'rosbag_play/pause_playback'
+    self.n_dig = 5
+    self.format = '{:.' + str(self.n_dig) + 'f}' # '{:.5f}'.format(num)
+    self.ini_time = 0
+
+    # Filenames
+    if rospy.has_param('out_dir'):
+      out_dir = rospy.get_param('out_dir')
+    else:
+      out_dir = os.environ['HOME']
+    odom_filename = out_dir + '/odom.txt'
+    scan_filename = out_dir + '/scan.txt'
+
+    # Waiting for available topics
+    to_wait = ['odom', 'scan']
+    while len(to_wait) > 0 and not rospy.is_shutdown():
+      rospy.loginfo('Waiting: [' + " ".join(to_wait) + ']')
+      current_topics = rospy.get_published_topics()
+      for t in current_topics:
+        topic_name = t[0][1:]
+        for w in to_wait:
+          if w == topic_name:
+            to_wait.remove(w)
+      rospy.sleep(1)
+
+    # Node subscribers
+    self.sub1 = rospy.Subscriber('odom', Odometry, self.odomCb)
+    self.sub2 = rospy.Subscriber('scan', LaserScan, self.scanCb)
+
+    # Open file for writing
+    self.odom_fd = open(odom_filename, 'w')
+    self.scan_fd = open(scan_filename, 'w')
+    
+    # Enable rosbag play (paused: false)
+    rospy.loginfo('Starting rosbag playback...')
+    rospy.wait_for_service(self.srv_name)
+    try: 
+      set_bool = rospy.ServiceProxy(self.srv_name, SetBool)
+      resp = set_bool(False)
+    except rospy.ServiceException, e:
+      rospy.logwarn('Service call failed: %s'%d)
+
+    rospy.loginfo('Writing files...')
+    rospy.loginfo(' - ' + odom_filename)
+    rospy.loginfo(' - ' + scan_filename)
+
+  def odomCb(self, msg):
+    if self.ini_time == 0:
+      self.ini_time = msg.header.stamp
+
+    quat = msg.pose.pose.orientation
+    orientation_list = [quat.x, quat.y, quat.z, quat.w]
+    (roll, pitch, yaw) = euler_from_quaternion (orientation_list)
+
+    #line = str(msg.header.stamp.secs) + '.' 
+    timestamp = msg.header.stamp.secs + msg.header.stamp.nsecs * 1e-9
+    line = self.format.format(timestamp) + '\t'
+    line += self.format.format(msg.pose.pose.position.x) + '\t'
+    line += self.format.format(msg.pose.pose.position.y) + '\t'
+    line += self.format.format(yaw) + '\t'
+    line += self.format.format(msg.twist.twist.linear.x) + '\t'
+    line += self.format.format(msg.twist.twist.angular.z) + '\n'
+    self.odom_fd.write(line)
+    #rospy.loginfo('- Odom log line: ' + line)
+
+  def scanCb(self, msg):
+    if self.ini_time == 0:
+      self.ini_time = msg.header.stamp
+    #line = str(msg.header.stamp.secs) + '.' 
+    timestamp = msg.header.stamp.secs + msg.header.stamp.nsecs * 1e-9
+    line = self.format.format(timestamp) + '\t'
+    for r in msg.ranges:
+      line += self.format.format(r) + '\t'
+    line = line[:-1] + '\n'
+    self.scan_fd.write(line)
+    #rospy.loginfo('- Scan log line: ' + line)
+
+if __name__ == '__main__':
+  rospy.init_node('odom_scan_log')
+  odom_scan_log = OdomScanLog()
+  rospy.spin()
+
